09129489ee1bf65c4f3e59b6730b17a8e4f605aa
Helios <adealumnonegligas@gmail.com>
2023-02-01 15:30:09 -0600

    conocimientos clase03

--- components/readTasks.js
+++ components/readTasks.js
@@ -13,3 +13,8 @@ export const readTasks = () => {
 }
 
 //fin de la clase03
+/* conocimientos clase03
+    1.-Recorrer el arreglo con forEach() de las tareas guardadas en LocalStorage
+    2.-Utilizar pipe
+    3.-Convertir las tareas almacenadas en formato string a objetos con JSON.parse()
+*/
