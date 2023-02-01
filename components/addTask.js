1122a1ee68fa5c417ff30c9bf11dfb721b2f4bf0
Helios <adealumnonegligas@gmail.com>
2023-02-01 14:54:30 -0600

    por secciones para que sea mas facil de seguir y entender

--- components/addTask.js
+++ components/addTask.js
@@ -3,33 +3,33 @@ import deleteIcon from './deleteIcon.js';
 
 
 export const addTask = (evento) => {
-    const list = document.querySelector('[data-list]');
-
     evento.preventDefault();
-    const tasklist = JSON.parse(localStorage.getItem("tasks")) || [];
-    console.log(tasklist);
+    
+    const list = document.querySelector('[data-list]');
     const input = document.querySelector('[data-form-input]');
     const calendar = document.querySelector("[data-form-date]");
+
     const value = input.value;
     const date = calendar.value;
     const dateFormat = moment(date).format('DD/MM/YYYY');
-    console.log(dateFormat);
+
     input.value = '';
+    calendar.value = '';
 
     const taskObj = {
         value,
         dateFormat
       }
 
+    const tasklist = JSON.parse(localStorage.getItem("tasks")) || [];
     tasklist.push({ value,dateFormat} );
-  
     localStorage.setItem("tasks",JSON.stringify(tasklist));
 
     const task = createTask(taskObj);
     list.appendChild(task);
   }
 
-  
+
   
   const createTask = ({ value,dateFormat }) => {
     
