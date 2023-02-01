b9617861c5d47f6761ed29bac355921afd726ed1
Helios <adealumnonegligas@gmail.com>
2023-02-01 12:31:57 -0600

    modularizacion de los componentes (aun no se corrige el problema de la sobreescritura del array para las tareas en localstorage)

--- script.js
+++ script.js
@@ -1,59 +1,8 @@
-import checkComplete from './components/checkComplete.js';
-import deleteIcon from './components/deleteIcon.js';
+import { addTask } from "./components/addTask.js";
 
 const btn = document.querySelector('[data-form-btn]');
 
-const addTask = (evento) => {// add la estructura html para que los elementos sean visibles para el usuario
-  const list = document.querySelector('[data-list]');
-  const task = createTask(evento);
-  list.appendChild(task);
 
-
-}
-
-const tasklist = [];// para almacenar array de las tareas del localstorage, (el const para los arreglos fija el tipo de dato que almacenan no sus valores, los valores pueden variar pero no el tipo de dato de los valores)
-
-const createTask = (evento) => {// crea la estructura html tomar datos y colocarlos donde corresponde (no los add por lo que no son visibles aun)
-  evento.preventDefault();
-  const input = document.querySelector('[data-form-input]');
-  const calendar = document.querySelector("[data-form-date]"); // captura el elemento input para entrada de fecha por el usuario (es todo el elemento)
-  const value = input.value;
-  const date = calendar.value;//captura el valor del elemento que es la fecha en si
-  const dateFormat = moment(date).format('DD/MM/YYYY');
-  /* console.log(dateFormat); */
-  const task = document.createElement('li');
-  task.classList.add('card');
-  input.value = '';
-  //backticks
-  const taskContent = document.createElement('div');
-
-  const taskObj = {
-    value,
-    dateFormat
-    /* lo mismo que 
-      value: value,
-      dateFormat: dateFormat */
-  }
-
-  tasklist.push(taskObj);// agregando al arreglo de las tareas el value,dateFormat
-
-  localStorage.setItem("tasks",JSON.stringify(tasklist));// persistenete sin importar si se cierra el navegador o pestania (recuerda que localStorage es un objeto de la API JS)
-
-
-
-  const titleTask = document.createElement('span');
-  titleTask.classList.add('task');
-  titleTask.innerText = value;
-  taskContent.appendChild(checkComplete());
-  taskContent.appendChild(titleTask);
-  // task.innerHTML = content;
-  const dateElement = document.createElement("span");
-  dateElement.innerHTML = dateFormat;
-  task.appendChild(taskContent);
-  task.appendChild(dateElement);
-  task.appendChild(deleteIcon());
-  return task;
-};
 
 //Arrow functions o funciones anonimas
 btn.addEventListener('click', addTask);
