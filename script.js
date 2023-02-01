adce26844e4f0e99277502ad7e55e69ad696b4df
Helios <adealumnonegligas@gmail.com>
2023-02-01 12:53:47 -0600

    clase 02 completada

--- script.js
+++ script.js
@@ -2,14 +2,11 @@ import { addTask } from "./components/addTask.js";
 
 const btn = document.querySelector('[data-form-btn]');
 
-
-
-//Arrow functions o funciones anonimas
 btn.addEventListener('click', addTask);
 
 /*  CONOCIMIENTOS:
 
-    1.- Utilizar el componente calendario
-    2.- Manipular fechas
-    3.- Destructurar objetos en javascript
+    1.- Guardar las tareas en el navegador utilizando Session Storage
+    2.- Almacenar las tareas con LocalStorage
+    3.- Transformar objetos en string utilizando JSON.stringify()
 */
